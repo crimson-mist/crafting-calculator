81b6ebee704410ac71912847f966a0593568cf1b
crimson-mist <103166755+crimson-mist@users.noreply.github.com>
2023-11-15 23:47:15 -0700

    Create recipes.py

---
+++ recipes.py
@@ -0,0 +1,142 @@
+recipes = {
+	"lv circuit": {
+		"base": False,
+		"subcomponents": {
+			"resistor": 2,
+			"steel casing": 1,
+			"vacuum tube": 2,
+			"circuit board": 1,
+			"red alloy cable": 3,
+		},
+		"quantity": 1,
+	},
+	"resistor": {
+		"base": False,
+		"subcomponents": {
+			"copper wire": 2,
+			"thin copper wire": 2,
+			"sticky resin": 2,
+			"charcoal dust": 1,
+		},
+		"quantity": 1,
+	},
+	"steel casing": {
+		"base": False,
+		"subcomponents": {
+			"steel ingot": 2,
+		},
+		"quantity": 3,
+	},
+	"vacuum tube": {
+		"base": False,
+		"subcomponents": {
+			"copper wire": 3,
+			"thin copper wire": 2,
+			"glass tube": 1,
+			"steel rod": 2,
+			"red alloy bolt": 1,
+		},
+		"quantity": 1,
+	},
+	"circuit board": {
+		"base": False,
+		"subcomponents": {
+			"copper wire": 8,
+			"wooden board": 1,
+		},
+		"quantity": 1,
+	},
+
+	"copper wire": {
+		"base": False,
+		"subcomponents": {
+			"copper ingot": 1,
+		},
+		"quantity": 2,
+	},
+	"thin copper wire": {
+		"base": False,
+		"subcomponents": {
+			"copper ingot": 1,
+		},
+		"quantity": 8,
+	},
+	"charcoal dust": {
+		"base": False,
+		"subcomponents": {
+			"charcoal": 1
+		},
+		"quantity": 1,
+	},
+	"red alloy bolt": {
+		"base": False,
+		"subcomponents": {
+			"red alloy ingot": 1,
+		},
+		"quantity": 2,
+	},
+	"red alloy ingot": {
+		"base": False,
+		"subcomponents": {
+			"copper ingot": 1,
+			"redstone": 4,
+		},
+		"quantity": 1,
+	},
+	"steel rod": {
+		"base": False,
+		"subcomponents": {
+			"steel ingot": 1,
+		},
+		"quantity": 1,
+	},
+	"red alloy cable": {
+		"base": False,
+		"subcomponents": {
+			"red alloy wire": 1,
+			"rubber sheet": 1,
+		},
+		"quantity": 1
+	},
+	"red alloy wire": {
+		"base": False,
+		"subcomponents": {
+			"red alloy ingot": 1,
+		},
+		"quantity": 2,
+	},
+
+	"copper ingot": {
+		"base": True,
+		"quantity": 1
+	},
+	"sticky resin": {
+		"base": True,
+		"quantity": 1,
+	},
+	"charcoal": {
+		"base": True,
+		"quantity": 1,
+	},
+	"steel ingot": {
+		"base": True,
+		"quantity": 1,
+	},
+	"wooden board": {
+		"base": True,
+		"quantity": 1,
+	},
+	"redstone": {
+		"base": True,
+		"quantity": 1,
+	},
+	"glass tube": {
+		"base": True,
+		"quantity": 1,
+	},
+	"rubber sheet": {
+		"base": True,
+		"quantity": 1,
+	},
+
+}
